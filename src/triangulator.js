9a12a8f1c8c56799c4e6225f80a4224071af0d08
jackw01 <jackw01@users.noreply.github.com>
2019-05-14 20:06:27 -0700

    point caching and babel for commonjs

--- src/triangulator.js
+++ src/triangulator.js
@@ -1,12 +1,14 @@
 // triangulator2 - SVG triangle art generator
 // Copyright 2019 jackw01. Released under the MIT License (see LICENSE for details).
 
+import seedrandom from 'seedrandom';
+import Delaunator from 'delaunator';
+import hash from 'object-hash';
+import chroma from 'chroma-js';
+import IterativePoissonDiscSampler from './poissondisc';
+import PerlinNoise from './perlin';
+
 const svg = typeof window === 'undefined' ? require('svg.js')(require('svgdom')) : require('svg.js');
-const seedrandom = require('seedrandom');
-const delaunator = require('delaunator');
-const chroma = require('chroma-js');
-const IterativePoissonDiscSampler = require('./poissondisc.js');
-const PerlinNoise = require('./perlin.js');
 
 // Utility stuff
 // Map value
@@ -24,6 +26,8 @@ let noiseGenerator = new PerlinNoise(rng);
 
 // triangulator2
 const triangulator = {
+  pointCache: [],
+  lastPointOptionsHash: '',
   GridMode: { Square: 1, Triangle: 2, Poisson: 3, Override: 4 },
   // Must return a scalar from 0 to 1
   ColorFunction: {
@@ -48,128 +52,139 @@ const triangulator = {
       return { gradientVector, gradientDirection };
     },
   },
-};
+  generate: function generate(input) {
+    const options = input || {
+      svgInput: false,
+      forceSVGSize: false,
+      seed: Math.random(),
+      width: 1920,
+      height: 1080,
+      gridMode: this.GridMode.Poisson,
+      gridOverridde: false,
+      cellSize: 100,
+      cellRandomness: 0.3,
+      colorOverride: false,
+      color: this.ColorFunction.Vertical,
+      colorPalette: ['#efee69', '#21313e'],
+      colorRandomness: 0.0,
+      quantizeSteps: 0,
+      useGradient: false,
+      gradient: this.GradientFunction.Random,
+      gradientNegativeFactor: 0.03,
+      gradientPositiveFactor: 0.03,
+      strokeColor: false,
+      strokeWidth: false,
+    };
 
-triangulator.generate = function generate(input) {
-  const options = input || {
-    svgInput: false,
-    seed: Math.random(),
-    width: 1920,
-    height: 1080,
-    gridMode: triangulator.GridMode.Poisson,
-    gridOverridde: false,
-    cellSize: 100,
-    cellRandomness: 0.3,
-    colorOverride: false,
-    color: triangulator.ColorFunction.Vertical,
-    colorPalette: ['#efee69', '#21313e'],
-    colorRandomness: 0.0,
-    quantizeSteps: 0,
-    useGradient: false,
-    gradient: triangulator.GradientFunction.Random,
-    gradientNegativeFactor: 0.03,
-    gradientPositiveFactor: 0.03,
-    strokeColor: false,
-    strokeWidth: false,
-  };
+    const gridOverdraw = 10;
 
-  const gridOverdraw = 10;
+    // Set up RNG
+    rng = seedrandom(`${options.seed}`);
+    noiseGenerator = new PerlinNoise(rng);
 
-  // Set up RNG
-  rng = seedrandom(`${options.seed}`);
-  noiseGenerator = new PerlinNoise(rng);
+    // Generate points
+    let points = [];
+    const pointOptionsHash = hash([options.seed, options.width, options.height, options.gridMode,
+      options.gridOverride, options.cellSize, options.cellRandomness]);
+    if (pointOptionsHash !== this.lastPointOptionsHash) {
+      this.lastPointOptionsHash = pointOptionsHash;
 
-  // Generate points
-  const points = [];
-  const cellRandomnessLimit = options.cellRandomness * options.cellSize;
-  if (options.gridMode === triangulator.GridMode.Square) {
-    for (let y = -gridOverdraw;
-      y < options.height + gridOverdraw + options.cellSize;
-      y += options.cellSize) {
-      for (let x = -gridOverdraw;
-        x < options.width + gridOverdraw + options.cellSize;
-        x += options.cellSize) {
-        points.push([
-          x + Math.floor(rng() * (2 * cellRandomnessLimit + 1)) - cellRandomnessLimit,
-          y + Math.floor(rng() * (2 * cellRandomnessLimit + 1)) - cellRandomnessLimit,
-        ]);
-      }
-    }
-  } else if (options.gridMode === triangulator.GridMode.Triangle) {
-    let r = 0;
-    for (let x = -gridOverdraw;
-      x < options.width + gridOverdraw + options.cellSize;
-      x += options.cellSize) {
-      for (let y = -gridOverdraw + options.cellSize / Math.sqrt(3) * (r % 2);
-        y < options.height + gridOverdraw + options.cellSize;
-        y += options.cellSize) {
-        points.push([
-          x + Math.floor(rng() * (2 * cellRandomnessLimit + 1)) - cellRandomnessLimit,
-          y + Math.floor(rng() * (2 * cellRandomnessLimit + 1)) - cellRandomnessLimit,
-        ]);
-      }
-      r++;
-    }
-  } else if (options.gridMode === triangulator.GridMode.Poisson) {
-    const sample = IterativePoissonDiscSampler(1.5 * options.width, 1.5 * options.height,
-      options.cellSize, rng);
-    let nextPoint = sample();
-    while (nextPoint) {
-      points.push([nextPoint[0] - 0.25 * options.width, nextPoint[1] - 0.25 * options.height]);
-      nextPoint = sample();
-    }
-  } else if (options.gridMode === triangulator.GridMode.Override) {
-    points.push(...options.gridOverride);
-  }
-
-  // Triangulate
-  const delaunay = delaunator.from(points);
-  const trianglePoints = [];
-  for (let i = 0; i < delaunay.triangles.length; i += 3) {
-    trianglePoints.push([
-      points[delaunay.triangles[i]], points[delaunay.triangles[i + 1]], points[delaunay.triangles[i + 2]],
-    ]);
-  }
-
-  // Convert input colors to chroma.js scale
-  const scale = chroma.scale(options.colorPalette).mode('hcl');
-
-  // Create SVG context and draw
-  const draw = svg(options.svgInput).size(options.width, options.height);
-
-  trianglePoints.forEach((tri) => {
-    // Find where the triangle's centroid lies on the gradient
-    const normX = map(tri.reduce((a, b) => a + b[0], 0) / 3, 0, options.width, 0, 1);
-    const normY = map(tri.reduce((a, b) => a + b[1], 0) / 3, 0, options.height, 0, 1);
-
-    // Get color/gradient for triangle
-    let color;
-
-    if (!options.colorOverride) {
-      // Determine where the color lies on the scale - quantize if necessary
-      let colorIndex = options.color(normX, normY) + (rng() - 0.5) * options.colorRandomness;
-      if (options.quantizeSteps) {
-        colorIndex = Math.round(colorIndex * options.quantizeSteps) / (options.quantizeSteps - 1);
+      const cellRandomnessLimit = options.cellRandomness * options.cellSize;
+      if (options.gridMode === triangulator.GridMode.Square) {
+        for (let y = -gridOverdraw;
+          y < options.height + gridOverdraw + options.cellSize;
+          y += options.cellSize) {
+          for (let x = -gridOverdraw;
+            x < options.width + gridOverdraw + options.cellSize;
+            x += options.cellSize) {
+            points.push([
+              x + Math.floor(rng() * (2 * cellRandomnessLimit + 1)) - cellRandomnessLimit,
+              y + Math.floor(rng() * (2 * cellRandomnessLimit + 1)) - cellRandomnessLimit,
+            ]);
+          }
+        }
+      } else if (options.gridMode === triangulator.GridMode.Triangle) {
+        let r = 0;
+        for (let x = -gridOverdraw;
+          x < options.width + gridOverdraw + options.cellSize;
+          x += options.cellSize) {
+          for (let y = -gridOverdraw + options.cellSize / Math.sqrt(3) * (r % 2);
+            y < options.height + gridOverdraw + options.cellSize;
+            y += options.cellSize) {
+            points.push([
+              x + Math.floor(rng() * (2 * cellRandomnessLimit + 1)) - cellRandomnessLimit,
+              y + Math.floor(rng() * (2 * cellRandomnessLimit + 1)) - cellRandomnessLimit,
+            ]);
+          }
+          r++;
+        }
+      } else if (options.gridMode === triangulator.GridMode.Poisson) {
+        const sample = IterativePoissonDiscSampler(1.5 * options.width, 1.5 * options.height,
+          options.cellSize, rng);
+        let nextPoint = sample();
+        while (nextPoint) {
+          points.push([nextPoint[0] - 0.25 * options.width, nextPoint[1] - 0.25 * options.height]);
+          nextPoint = sample();
+        }
+      } else if (options.gridMode === triangulator.GridMode.Override) {
+        points.push(...options.gridOverride);
       }
 
-      if (!options.useGradient) { // Use solid color
-        color = scale(colorIndex).hex();
-      } else { // Generate gradient vector and direction
-        const { gradientVector, gradientDirection } = options.gradient(tri, normX, normY);
-        color = draw.gradient('linear', (stop) => {
-          stop.at(0, scale(colorIndex - options.gradientNegativeFactor * gradientDirection).hex());
-          stop.at(1, scale(colorIndex + options.gradientPositiveFactor * gradientDirection).hex());
-        }).from(0.0, 0.0).to(...gradientVector);
-      }
+      this.pointCache = points;
     } else {
-      color = options.colorOverride(normX, normY);
+      points = this.pointCache;
     }
 
-    draw.polygon(tri.map(p => p.join(',')).join(' '))
-      .fill(color).stroke({ color: options.strokeColor || color, width: options.strokeWidth || 1 });
-  });
+    // Triangulate
+    const delaunay = Delaunator.from(points);
+    const trianglePoints = [];
+    for (let i = 0; i < delaunay.triangles.length; i += 3) {
+      trianglePoints.push([
+        points[delaunay.triangles[i]], points[delaunay.triangles[i + 1]], points[delaunay.triangles[i + 2]],
+      ]);
+    }
 
-  return draw.svg();
+    // Convert input colors to chroma.js scale
+    const scale = chroma.scale(options.colorPalette).mode('hcl');
+
+    // Create SVG context and draw
+    const draw = svg(options.svgInput);
+    if (options.forceSVGSize) draw.size(options.width, options.height);
+
+    trianglePoints.forEach((tri) => {
+      // Find where the triangle's centroid lies on the gradient
+      const normX = map(tri.reduce((a, b) => a + b[0], 0) / 3, 0, options.width, 0, 1);
+      const normY = map(tri.reduce((a, b) => a + b[1], 0) / 3, 0, options.height, 0, 1);
+
+      // Get color/gradient for triangle
+      let color;
+
+      if (!options.colorOverride) {
+        // Determine where the color lies on the scale - quantize if necessary
+        let colorIndex = options.color(normX, normY) + (rng() - 0.5) * options.colorRandomness;
+        if (options.quantizeSteps) {
+          colorIndex = Math.round(colorIndex * options.quantizeSteps) / (options.quantizeSteps - 1);
+        }
+
+        if (!options.useGradient) { // Use solid color
+          color = scale(colorIndex).hex();
+        } else { // Generate gradient vector and direction
+          const { gradientVector, gradientDirection } = options.gradient(tri, normX, normY);
+          color = draw.gradient('linear', (stop) => {
+            stop.at(0, scale(colorIndex - options.gradientNegativeFactor * gradientDirection).hex());
+            stop.at(1, scale(colorIndex + options.gradientPositiveFactor * gradientDirection).hex());
+          }).from(0.0, 0.0).to(...gradientVector);
+        }
+      } else {
+        color = options.colorOverride(normX, normY);
+      }
+
+      draw.polygon(tri.map(p => p.join(',')).join(' '))
+        .fill(color).stroke({ color: options.strokeColor || color, width: options.strokeWidth || 1 });
+    });
+
+    return draw.svg();
+  },
 };
 
-module.exports = triangulator;
+export default triangulator;
--- test/test.js
+++ test/test.js
@@ -1,24 +1,24 @@
 const fs = require('fs');
 const window = require('svgdom');
 const svg2Img = require('svg2img');
-const triangulator = require('../src/triangulator.js');
+const Triangulator = require('../src/index.js');
 
-const svgString = triangulator.generate({
+const svgString = Triangulator.generate({
   svgInput: window.document.documentElement,
   seed: 4,
   width: 3840,
   height: 2160,
-  gridMode: triangulator.GridMode.Poisson,
+  gridMode: Triangulator.GridMode.Poisson,
   gridOverride: false,
   cellSize: 150,
   cellRandomness: 0.2,
   colorOverride: false,
-  color: triangulator.ColorFunction.RadialFromBottom,
+  color: Triangulator.ColorFunction.RadialFromBottom,
   colorPalette: ['#e7a71d', '#dc433e', '#9e084b', '#41062f'],
   colorRandomness: 0.15,
   quantizeSteps: 0,
   useGradient: true,
-  gradient: triangulator.GradientFunction.Random,
+  gradient: Triangulator.GradientFunction.Random,
   gradientNegativeFactor: 0.03,
   gradientPositiveFactor: 0.03,
   strokeColor: false,
@@ -35,3 +35,29 @@ svg2Img(svgString, (err, buffer) => {
 fs.writeFile('out.svg', svgString, (err) => {
   if (err) throw err;
 });
+
+console.log(1);
+
+const svgString2 = Triangulator.generate({
+  svgInput: window.document.documentElement,
+  seed: 4,
+  width: 3840,
+  height: 2160,
+  gridMode: Triangulator.GridMode.Poisson,
+  gridOverride: false,
+  cellSize: 150,
+  cellRandomness: 0.2,
+  colorOverride: false,
+  color: Triangulator.ColorFunction.RadialFromBottom,
+  colorPalette: ['#e7a71d', '#dc433e', '#9e084b', '#41062f'],
+  colorRandomness: 0.15,
+  quantizeSteps: 0,
+  useGradient: true,
+  gradient: Triangulator.GradientFunction.Random,
+  gradientNegativeFactor: 0.03,
+  gradientPositiveFactor: 0.03,
+  strokeColor: false,
+  strokeWidth: 1,
+});
+
+console.log(2);
